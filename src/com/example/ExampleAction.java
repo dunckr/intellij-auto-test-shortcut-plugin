7f1c2bbb5216963a0618b1434394b47699a4d10c
Duncan Beaton <dunckr@gmail.com>
2021-01-31 21:24:05 +0000

    experimenting with different interfaces

--- src/com/example/ExampleAction.java
+++ src/com/example/ExampleAction.java
@@ -21,7 +21,11 @@ import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowId;
+import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -39,74 +43,44 @@ public class ExampleAction extends AnAction implements AnAction.TransparentUpdat
         try {
             var project = event.getData(PlatformDataKeys.PROJECT);
 
-//            ExecutionManager executionManager = ExecutionManager.getInstance(project);
+            if (project != null) {
+                ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
 
-//            Content content = RunDashboardManager.getInstance(project).getDashboardContentManager().getSelectedContent();
-//            RunContentDescriptor descriptor = RunContentManagerImpl.getRunContentDescriptorByContent(content);
+                ToolWindow window = toolWindowManager.getToolWindow(ToolWindowId.RUN);
+                LOG.info("have window");
+                ContentManager contentManager = window.getContentManager();
+                LOG.info("have contentManager");
 
-//            JComponent component = content.getComponent();
+                for (Content content : contentManager.getContents()) {
+                    if (content != null) {
+                        RunContentDescriptor descriptor = content.getUserData(RunContentDescriptor.DESCRIPTOR_KEY);
 
-//            ExecutionEnvironment environment = LangDataKeys.EXECUTION_ENVIRONMENT.getData(DataManager.getInstance().getDataContext());
-//            ExecutionEnvironment environment = LangDataKeys.EXECUTION_ENVIRONMENT.getData(DataManager.getInstance().getDataContext(component));
-
-
-//             RunContentDescriptor descriptor ExecutionEnvironment environment
-//            AutoTestManager.getInstance(project).setAutoTestEnabled(descriptor, environment, true);
-
-
-//            var isEnabled = new ToggleAutoTestAction().isSelected(event);
-//
-//            LOG.info("ad");
-//
-////            RunContentDescriptor descriptor = event.getData(LangDataKeys.RUN_CONTENT_DESCRIPTOR);
-////            ExecutionEnvironment environment = event.getData(LangDataKeys.EXECUTION_ENVIRONMENT);
-//
-//
-//            for (ProcessHandler processHandler : executionManager.getRunningProcesses()) {
-//                processHandler.getUserData()
-//
-//                LOG.info("ad");
-//                processHandler.con
-//            }
-//
-////            ExecutionManager.getInstance(project).contentManager.selectedContent
-//
-            RunConfiguration[] allConfigurations = RunManager.getInstance(project).getAllConfigurations();
-//
-////            new RunContentDescriptor().isAc
-//
-            for (RunConfiguration configuration : allConfigurations) {
-
-                configuration.
-//                RunConfiguration configuration = RunConfiguration.DATA_KEY.getData(dataContext);
-//                ExecutionEnvironment environment = configuration.getData(LangDataKeys.EXECUTION_ENVIRONMENT);
-//                ProgramRunner runner = RunnerRegistry.getInstance().getRunner(DefaultRunExecutor.EXECUTOR_ID, configuration);
-//
-//                LOG.info("ad");
-//
+                        if (descriptor != null) {
+                            var isEnabled = AutoTestManager.getInstance(project).isAutoTestEnabled(descriptor);
+                            LOG.info("isEnabled");
+                            LOG.info((Boolean.toString(isEnabled)));
+                        } else {
+                            LOG.info("descriptor is null");
+                        }
+                    } else {
+                        LOG.info("content is null");
+                    }
+                }
+            } else {
+                LOG.info("project is null");
             }
 
-
-//
-//            if (project != null && descriptor != null && environment != null) {
-//                LOG.debug("we are not null....");
-//                new ToggleAutoTestAction().setSelected(event, true);
-//                // AutoTestManager.getInstance(project).setAutoTestEnabled(descriptor, environment, true);
-//                // LOG.debug("completed....");
-//            } else {
-//                LOG.debug("NULL....");
-//            }
             LOG.info("done");
         } catch (Exception e) {
-            LOG.error("error!!!!!!!!!!!");
-            LOG.debug(e);
+            LOG.info("error!!!!!!!!!!!");
+            LOG.info(e);
         }
     }
 
     @Override
     public void update(AnActionEvent e) {
         Project project = e.getProject();
-        LOG.debug("updating");
+        LOG.debug("update");
         e.getPresentation().setEnabledAndVisible(project != null);
     }
 }
